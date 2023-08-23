89b4797d241f456bd1e170a9b4cef6c77d15e8f2
Carlos Andres Osorio Alcalde <carlosandresosorioalcalde@Carloss-MacBook-Air.local>
2023-08-22 19:07:09 -0500

    Prompt engineering

--- assistantbot/ai/text/prompts/image_captioning.py
+++ assistantbot/ai/text/prompts/image_captioning.py
@@ -1,24 +1,21 @@
-IMAGE_CAPTION_PROMPT = """
-You just received an image from Carlos.
+IMAGE_CAPTION_PROMPT_REQUEST = """
+Assume that you just received an image from Carlos.
 This is a description of the image: {caption}
 
 {image_message}
 
-If there is a Carlos's request in this message, answer it.
-
-If not, create a message describing the image.
-Be funny, be creative, and use as much emojis as you want, but keep the
-message short and simple. Don't mention again the Carlos' request.
+Use the information of the caption to answer the request.
 
 Your answer:
 """
 
-USER_PROMPT_TEMPLATE_CAPTIONING = """
-What do you think about this image?
-"""
+IMAGE_CAPTION_PROMPT_WITHOUT_REQUEST = """
+Assume that you just received an image from Carlos.
+This is a description of the image: {caption}
 
-AUTOMATIC_IMAGE_CAPTION = """
+Create a message describing the image.
+Be funny, be creative, and use as much emojis as you want, but keep the
+message short and simple.
 
-This is the caption for the image:
-{caption}
+Your answer:
 """
